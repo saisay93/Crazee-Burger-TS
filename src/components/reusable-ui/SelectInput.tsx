610b4ec6b8a89ae89926b2d9e59e59bc30ef565c
saisay93 <saisay@hotmail.com>
2024-12-04 04:52:14 +0100

    ♻️ TSE-02: REUSABLE-UI > Migration of <SelectInput /> to TS

--- src/components/reusable-ui/SelectInput.tsx
+++ src/components/reusable-ui/SelectInput.tsx
@@ -1,8 +1,23 @@
-import React from "react"
-import styled from "styled-components"
-import { theme } from "../../theme"
+import React from "react";
+import styled from "styled-components";
+import { theme } from "../../theme";
 
-export default function SelectInput({
+
+interface Option {
+  optionValue: string; 
+  label: string; 
+}
+
+interface SelectInputProps {
+  options: Option[]; 
+  value: string; 
+  name: string; 
+  Icon?: React.ReactNode; 
+  className?: string; 
+  onChange?: React.ChangeEventHandler<HTMLSelectElement>; 
+}
+
+const SelectInput = ({
   options,
   value,
   name,
@@ -10,7 +25,7 @@ export default function SelectInput({
   className,
   onChange,
   ...restProps
-}) {
+}: SelectInputProps) => {
   return (
     <SelectInputStyled className={className}>
       {Icon && <div className="icon">{Icon}</div>}
@@ -22,11 +37,10 @@ export default function SelectInput({
         ))}
       </select>
     </SelectInputStyled>
-  )
-}
+  );
+};
 
 const SelectInputStyled = styled.div`
-  /* border: 1px solid yellow; */
   background-color: ${theme.colors.background_white};
   border-radius: ${theme.borderRadius.round};
   display: flex;
@@ -34,15 +48,13 @@ const SelectInputStyled = styled.div`
   padding: 8px 16px;
 
   .icon {
-    /* border: 1px solid red; */
-    font-size: ${theme.fonts.P1};
+    font-size: ${theme.fonts.size.P1};
     margin-right: 13px;
     color: ${theme.colors.greyBlue};
-    display: flex; // centre verticalement l'icône dans le champ select
+    display: flex;
   }
 
   select {
-    /* border: 1px solid blue; */
     background: ${theme.colors.background_white};
     border: none;
     font-size: ${theme.fonts.size.SM};
@@ -50,12 +62,6 @@ const SelectInputStyled = styled.div`
     width: 100%;
     outline: 0;
   }
-` // commente/décommente une ligne CSS pour connaître son effet sur le composant
+`;
 
-// Attention à deux props "value":
-// 1️⃣ value dans <select/> (valeur selectionnée) ==> onChange est lié à cette value
-// 2️⃣ value dans <option/> (valeur disponible)
-// const options = [
-//   { optionValue: "", label: "", selected: true },
-//   { optionValue: "", label: "", selected: false },
-// ]
+export default SelectInput;
