43c645d324613b94a0b2e22331b87660c38f45ec
saisay93 <saisay@hotmail.com>
2024-12-06 03:49:29 +0100

    ♻️ TSE-02: REUSABLE-UI > Change types props interface to type ToggleButton.tsx

--- src/components/reusable-ui/Sticker.tsx
+++ src/components/reusable-ui/Sticker.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { theme } from "../../theme";
 
 type StickerProps = {
-	label: string;
+	label?: string;
 	className?: string;
 }
 
--- src/components/reusable-ui/ToggleButton.tsx
+++ src/components/reusable-ui/ToggleButton.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import styled from "styled-components";
 import { theme } from "../../theme";
 
-interface ToggleButtonProps {
+type ToggleButtonProps = {
 	isChecked: boolean;
-	onToggle: () => void;
+	onToggle?: React.ChangeEventHandler<HTMLInputElement>;
 	labelIfChecked?: string;
 	labelIfUnchecked?: string;
 }
