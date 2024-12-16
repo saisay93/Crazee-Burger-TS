cfd3be5778227579bbb9a5cac51cfac1de51cd1c
saisay93 <saisay@hotmail.com>
2024-12-16 19:06:13 +0100

    ♻️ TSE-05: SRC > API > Add user.ts

--- src/constants/product.ts
+++ src/constants/product.ts
@@ -1,19 +1,19 @@
-import { MenuProduct } from "../Types/MenuProduct"
+import { MenuProduct } from "../Types/Product";
 
 export const EMPTY_PRODUCT: MenuProduct = {
-  id: "",
-  title: "",
-  imageSource: "",
-  price: 0,
-  isAvailable: true,
-  isPublicised: false,
-}
+	id: "",
+	title: "",
+	imageSource: "",
+	price: 0,
+	isAvailable: true,
+	isPublicised: false,
+};
 
-export const IMAGE_COMING_SOON = "/images/coming-soon.png"
-export const IMAGE_NO_STOCK = "/images/stock-epuise.png"
+export const IMAGE_COMING_SOON = "/images/coming-soon.png";
+export const IMAGE_NO_STOCK = "/images/stock-epuise.png";
 
-export  const BASKET_MESSAGE = {
-  EMPTY: "Votre commande est vide.",
-  LOADING: "Chargement en cours...",
-  NOT_AVAILABLE: "Non disponible",
-} as const
+export const BASKET_MESSAGE = {
+	EMPTY: "Votre commande est vide.",
+	LOADING: "Chargement en cours...",
+	NOT_AVAILABLE: "Non disponible",
+} as const;
