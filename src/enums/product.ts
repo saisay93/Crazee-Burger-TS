f254792c380c17b824d57b72a6160fb2db7dcdff
saisay93 <saisay@hotmail.com>
2024-12-16 17:50:10 +0100

    ♻️ TSE-05: SRC > ENUMS > Add products.ts

--- src/enums/product.ts
+++ src/enums/product.ts
@@ -1,17 +1,19 @@
-export const EMPTY_PRODUCT = Object.freeze({
+import { MenuProduct } from "../Types/MenuProduct"
+
+export const EMPTY_PRODUCT: MenuProduct = {
   id: "",
   title: "",
   imageSource: "",
   price: 0,
   isAvailable: true,
   isPublicised: false,
-})
+}
 
 export const IMAGE_COMING_SOON = "/images/coming-soon.png"
 export const IMAGE_NO_STOCK = "/images/stock-epuise.png"
 
-export const BASKET_MESSAGE = {
+export  const BASKET_MESSAGE = {
   EMPTY: "Votre commande est vide.",
   LOADING: "Chargement en cours...",
   NOT_AVAILABLE: "Non disponible",
-}
+} as const
