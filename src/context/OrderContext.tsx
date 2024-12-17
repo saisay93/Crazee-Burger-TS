17f3b6ac2365c2be4991703dee981dc67af3fae8
saisay93 <saisay@hotmail.com>
2024-12-17 16:41:52 +0100

    ♻️ TSE-07-CONTEXT: Add Context.tsx (Part I)

--- src/context/OrderContext.tsx
+++ src/context/OrderContext.tsx
@@ -1,9 +1,11 @@
-import { createContext, useContext, useEffect, useRef, useState } from "react"
+import { PropsWithChildren, createContext, useContext, useEffect, useRef, useState } from "react"
 import { findObjectById } from "../utils/array";
 import {useMenu} from "../hooks/useMenu"
 import {useBasket} from "../hooks/useBasket"
 import { useParams } from "react-router-dom";
 import { EMPTY_PRODUCT } from "../constants/product";
+import { ADMIN_TAB_LABEL } from "../constants/tab";
+//@ts-ignore
 import { initialiseUserSession } from "../components/pages/order/helpers/initialiseUserSession";
 
 // 1. Creation du context
@@ -41,26 +43,28 @@ import { initialiseUserSession } from "../components/pages/order/helpers/initial
 
 // 2. Installation du context
 
-export const OrderContextProvider = ({children}) => {
+export const OrderContextProvider = ({children}: PropsWithChildren) => {
 
   const [isModeAdmin, setIsModeAdmin] = useState(false);
 	const [isCollapsed, setIsCollapsed] = useState(false);
-	const [currentTabSelected, setCurrentTabSelected] = useState("add");
+	const [currentTabSelected, setCurrentTabSelected] = useState<ADMIN_TAB_LABEL>(ADMIN_TAB_LABEL.ADD);
 	const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT);
 	const [productSelected, setProductSelected] = useState(EMPTY_PRODUCT);
-	const titleEditRef = useRef();
+	const titleEditRef = useRef<HTMLInputElement>(null);
 	const { menu, setMenu, handleAdd, handleDelete, handleEdit, resetMenu } =
 		useMenu();
 	const { basket, setBasket, handleAddToBasket, handleDeleteBasketProduct } =
 		useBasket();
 	const { username } = useParams();
 
-	const handleProductSelected = async (idProductClicked) => {
+	const handleProductSelected = async (idProductClicked: string) => {
+		if(!menu) return
 		const productClickedOn = findObjectById(idProductClicked, menu);
+		if(!productClickedOn) return
 		await setIsCollapsed(false);
-		await setCurrentTabSelected("edit");
+		await setCurrentTabSelected(ADMIN_TAB_LABEL.EDIT);
 		await setProductSelected(productClickedOn);
-		titleEditRef.current.focus();
+		titleEditRef.current?.focus();
 	};
 
 	useEffect(() => {
