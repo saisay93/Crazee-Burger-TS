e03b6d90ef03df7d992c77c6c863887c45002dc5
saisay93 <saisay@hotmail.com>
2024-12-17 14:45:27 +0100

    ♻️ F017-REFACTO-USECONTEXT-AVANCE: Create & Install new advanced Context

--- src/context/OrderContext.jsx
+++ src/context/OrderContext.jsx
@@ -1,6 +1,13 @@
-import { createContext } from "react"
+import { createContext, useEffect, useRef, useState } from "react"
+import { findObjectById } from "../utils/array";
+import {useMenu} from "../hooks/useMenu"
+import {useBasket} from "../hooks/useBasket"
+import { useParams } from "react-router-dom";
+import { EMPTY_PRODUCT } from "../constants/product";
+import { initialiseUserSession } from "../components/pages/order/helpers/initialiseUserSession";
 
-export default createContext({
+// 1. Creation du context
+ const OrderContext = createContext({
   username: "",
   isModeAdmin: false,
   setIsModeAdmin: () => {},
@@ -30,3 +37,70 @@ export default createContext({
   handleAddToBasket: () => {},
   handleDeleteBasketProduct: () => {},
 })
+
+export default OrderContext
+
+
+// 2. Installation du context
+
+export const OrderContextProvider = ({children}) => {
+
+  const [isModeAdmin, setIsModeAdmin] = useState(false);
+	const [isCollapsed, setIsCollapsed] = useState(false);
+	const [currentTabSelected, setCurrentTabSelected] = useState("add");
+	const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT);
+	const [productSelected, setProductSelected] = useState(EMPTY_PRODUCT);
+	const titleEditRef = useRef();
+	const { menu, setMenu, handleAdd, handleDelete, handleEdit, resetMenu } =
+		useMenu();
+	const { basket, setBasket, handleAddToBasket, handleDeleteBasketProduct } =
+		useBasket();
+	const { username } = useParams();
+
+	const handleProductSelected = async (idProductClicked) => {
+		const productClickedOn = findObjectById(idProductClicked, menu);
+		await setIsCollapsed(false);
+		await setCurrentTabSelected("edit");
+		await setProductSelected(productClickedOn);
+		titleEditRef.current.focus();
+	};
+
+	useEffect(() => {
+		initialiseUserSession(username, setMenu, setBasket);
+	}, []);
+
+  const orderContextValue = {
+		username,
+		isModeAdmin,
+		setIsModeAdmin,
+		isCollapsed,
+		setIsCollapsed,
+		currentTabSelected,
+		setCurrentTabSelected,
+		menu,
+		handleAdd,
+		handleDelete,
+		resetMenu,
+		newProduct,
+		setNewProduct,
+		productSelected,
+		setProductSelected,
+		handleEdit,
+		titleEditRef,
+		basket,
+		handleAddToBasket,
+		handleDeleteBasketProduct,
+		handleProductSelected,
+	}
+
+
+  return (
+    <OrderContext.Provider value={orderContextValue}>
+      {children}
+    </OrderContext.Provider>
+  )
+  
+}
+
+// 3. Consommation du context
+
